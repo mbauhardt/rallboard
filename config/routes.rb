1d4585caf3253cd5b558403d5d3e791e855761ad
Marko Bauhardt <mb@datameer.com>
2011-07-17 19:41:30 +0200

    switch to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rallboard::Application.routes.draw do
-  get "home/home"
+  get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +51,7 @@ Rallboard::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "home#home"
+	root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
